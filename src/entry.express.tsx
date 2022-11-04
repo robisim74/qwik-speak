d03f25eefd16a6e1e361b1e9da57b946a73323d8
Roberto Simonetti <robisim74@gmail.com>
2022-11-04 22:33:51 +0100

    Upgrade to Qwik v0.12.1

--- src/entry.express.tsx
+++ src/entry.express.tsx
@@ -1,23 +1,31 @@
-import { qwikCity } from '@builder.io/qwik-city/middleware/node';
-import express from 'express';
-import { fileURLToPath } from 'url';
-import { join } from 'path';
+import { createQwikCity } from '@builder.io/qwik-city/middleware/node';
+import qwikCityPlan from '@qwik-city-plan';
 import render from './entry.ssr';
+import express from 'express';
+import { fileURLToPath } from 'node:url';
+import { join } from 'node:path';
+import compression from 'compression';
 
 // Directories where the static assets are located
 const distDir = join(fileURLToPath(import.meta.url), '..', '..', 'dist');
 const buildDir = join(distDir, 'build');
 
+// Allow for dynamic port
+const PORT = process.env.PORT ?? 8080;
+
 // Create the Qwik City express middleware
-const { router, notFound } = qwikCity(render);
+const { router, notFound } = createQwikCity({ render, qwikCityPlan });
 
 // Create the express server
 // https://expressjs.com/
 const app = express();
 
+// Enable gzip compression
+app.use(compression());
+
 // Static asset handlers
 // https://expressjs.com/en/starter/static-files.html
-app.use('/build', express.static(buildDir, { immutable: true, maxAge: '1y' }));
+app.use(`/build`, express.static(buildDir, { immutable: true, maxAge: '1y' }));
 app.use(express.static(distDir, { redirect: false }));
 
 // Use Qwik City's page and endpoint request handler
@@ -27,7 +35,7 @@ app.use(router);
 app.use(notFound);
 
 // Start the express server
-app.listen(8080, () => {
+app.listen(PORT, () => {
   /* eslint-disable */
-  console.log(`http://localhost:8080/`);
+  console.log(`Server starter: http://localhost:${PORT}/`);
 });
