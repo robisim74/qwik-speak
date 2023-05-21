d96cc7e846a50bcc0bf46b6c1de786807f8a98cc
Roberto Simonetti <robisim74@gmail.com>
2023-05-21 16:11:28 +0200

    Update cloudflare

--- src/entry.cloudflare-pages.tsx
+++ src/entry.cloudflare-pages.tsx
@@ -16,7 +16,7 @@ import { manifest } from '@qwik-client-manifest';
 import render from './entry.ssr';
 
 declare global {
-  interface QwikCityPlatform extends PlatformCloudflarePages { }
+  interface QwikCityPlatform extends PlatformCloudflarePages {}
 }
 
 const fetch = createQwikCity({ render, qwikCityPlan, manifest });
